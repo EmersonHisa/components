3e0a7bfcad2aecf4b8dcf5759f1a876cad9c98eb
Hsiamomomo <105873493+EmersonHisa@users.noreply.github.com>
2022-09-09 03:06:02 +0100

    9.9

---
+++ 018_counter/app.js
@@ -0,0 +1,25 @@
+const btns = document.querySelectorAll(".btn");
+const value = document.getElementById("value");
+let count = 0;
+btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
+    if (count > 0) {
+      value.style.color = "green";
+    }
+    if (count < 0) {
+      value.style.color = "red";
+    }
+    if (count === 0) {
+      value.style.color = "black";
+    }
+    value.textContent = count;
+  });
+});
